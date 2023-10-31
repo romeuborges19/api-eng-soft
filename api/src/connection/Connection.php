4ec248d1a909b5d435d2bf4b6738735cb9aeccde
Romeu Miranda Borges <romeuborges19@gmail.com>
2023-10-31 14:58:43 -0300

    first commit

---
+++ api/src/connection/Connection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Connection;
+use PDOExeption;
+
+class Connection{
+	function connect() {
+		$host = "localhost";
+		$dbName = "api_eng_soft";
+		$user = "postgres";
+		$password = "romeulindo";
+		$port = "5432";
+
+		try { 
+			$connection = pg_connect("host=$host port=$port dbname=$dbName user=$user password=$password");
+			return $connection;
+		} catch (\PDOExeption $error) {
+			echo "Erro na conexão: " . $error->getMessage();
+		}
+	}
+}
+
+?>
